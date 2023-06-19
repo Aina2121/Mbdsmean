c7ccc86b675c8fd560e7ba465b56f828a58a8e62
Aina2121 <70892748+Aina2121@users.noreply.github.com>
2023-06-19 17:59:25 +0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   hmr: false,
-  apiUrl: 'https://mean-mbds-node3839.onrender.com/api'
+  apiUrl: 'https://mean-mbds-node3839.onrender.com/api/'
 };
